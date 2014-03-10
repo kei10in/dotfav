2ecd90eebc2e703b3e7527fe78d50b7784ea1369
kei10in <kei10in@gmail.com>
2014-03-11 06:35:38 +0900

    create test for symlink with config file

--- tests/test_symlink.py
+++ tests/test_symlink.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+import json
+
 from utils import *
 
 import dotfav
@@ -27,15 +29,34 @@ class TestScenario1(object):
         self.run_dotfav_symlink()
         self.dotfav_symlink_creates_a_symlinked_directory()
 
+    def test_symlink_with_map_file(self):
+        self.given_there_are_dotfiles_home_directory_indotfiles()
+        self.when_dotfiles_contains_config_file([{
+                'os': ['darwin'],
+                'target': {'file': 'file.darwin'}
+            }])
+        self.when_dotfiles_home_contains_a_file_named('file.darwin')
+        self.run_dotfav_symlink()
+        self.dotfav_symlink_creates_a_symlink(
+            test_dotfiles_home.join('file.darwin'),
+            test_home.join('file'))
+
     def given_there_are_dotfiles_home_directory_in_dotfiles(self):
         create_test_temp_directories()
         assert test_dotfiles_home.isdir()
+
+    def when_dotfiles_contains_map_file(self, config):
+        with open(test_dotfiles.join('dotfav.config'), mode='w') as f:
+            json.dump(config, f)
 
     def when_dotfiles_home_directory_contains_no_files(self):
         pass
 
     def when_dotfiles_home_directory_contains_a_file(self):
-        create_file_into_dotfiles_home('file')
+        self.when_dotfiles_home_directory_contains_a_file_named('file')
+
+    def when_dotfiles_home_directory_contains_a_file_named(self, name):
+        create_file_into_dotfiles_home(name)
 
     def when_dotfiles_home_directory_contains_a_directory(self):
         create_directory_into_dotfiles_home('directory')
@@ -58,6 +79,10 @@ class TestScenario1(object):
         assert any(
             [path.isdir() and self.is_symlink_of_dotfiles_home(path)
              for path in test_home.children()])
+
+    def dotfav_symlink_creates_a_symlink(self, src, dst):
+        assert dst.islink()
+        assert dst.realpath() == src
 
     def is_symlink_of_dotfiles_home(self, symlinkpath):
         return (symlinkpath.islink() and
